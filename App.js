e908519e9b98b6bf6486be02e3cffc35e34b3ec2
Kenneth Lai <klnyc6@gmail.com>
2019-10-06 22:13:22 -0400

    added drawer!

--- App.js
+++ App.js
@@ -3,14 +3,25 @@ import { StyleSheet, Text, View } from 'react-native';
 import Map from './components/Map'
 import Login from './components/Login'
 import Home from './components/Home'
-import Links from './components/Links'
 import SettingsScreen from './components/SettingsScreen';
+import { createDrawerNavigator } from 'react-navigation-drawer'
+import { createAppContainer } from 'react-navigation'
+
+const drawer = createDrawerNavigator({
+  Home,
+  Login,
+  Map,
+  SettingsScreen
+})
+
+const Drawer = createAppContainer(drawer)
 
 export default function App() {
   return (
     <View style={styles.container}>
       {/* <Login /> */}
-      <Home />
+      {/* <Home /> */}
+      <Drawer />
     </View>
   );
 }
