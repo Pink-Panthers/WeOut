2996d8996e9ed94fef879ef5caaa0ccdfc754cbc
Kenneth Lai <klnyc6@gmail.com>
2019-10-07 18:12:47 -0400

    added logo in drawer

--- components/DrawerDesign.js
+++ components/DrawerDesign.js
@@ -1,12 +1,8 @@
 import React from 'react'
-import { View, Text, Platform, Dimensions, StyleSheet} from 'react-native'
-import { TouchableOpacity } from 'react-native-gesture-handler'
-
-const width = Dimensions.get('window').width
-const height = Dimensions.get('window').height
+import { View, Text, StyleSheet, Image } from 'react-native'
+import { TouchableOpacity, ScrollView } from 'react-native-gesture-handler'
 
 export default class DrawerDesign extends React.Component {
- 
     navLink(nav, text) {
         return (
             <TouchableOpacity 
@@ -17,16 +13,24 @@ export default class DrawerDesign extends React.Component {
             </TouchableOpacity>
         )
     }
+    
     render() {
         return (
             <View style={styles.container}>
                 <View style={styles.top}>
-                    <Text style={{paddingTop: 40}}>WeOut</Text>
+                    <Image
+                        style={styles.logo}
+                        source={require("../assets/weOut.png")}
+                    />
                 </View>
-                <View style={styles.bottom}>
+                <ScrollView style={styles.bottom}>
+                <View>
                     {this.navLink('Home', 'Home')}
                     {this.navLink('Circle', 'Circle')}
+                    {this.navLink('Map', 'Map')}
+                    {this.navLink('Login', 'Login')}
                 </View>
+                </ScrollView>
             </View>
         )
     }
@@ -35,24 +39,32 @@ export default class DrawerDesign extends React.Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: 'red'
+        backgroundColor: '#fff'
     },
     top: {
-        height: 100,
-        backgroundColor: 'yellow',
+        height: 140,
+        backgroundColor: '#ff7f50',
+        alignItems: 'flex-end',
+        justifyContent: 'flex-end'
     },
     bottom: {
         flex: 1,
-        backgroundColor: 'white',
+        backgroundColor: 'tan',
         paddingTop: 20,
         paddingBottom: 450
     },
     link: {
         flex: 1,
         fontSize: 20,
-        padding: 6,
-        paddingLeft: 14,
-        margin: 5,
+        padding: 2,
+        paddingLeft: 12,
+        margin: 6,
         textAlign: 'left'
+    },
+    logo: {
+        paddingTop: 40,
+        paddingLeft: 12,
+        width: 100,
+        height: 100
     }
 })
