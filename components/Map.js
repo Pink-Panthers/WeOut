a440af27ab2120ca43874b981c65a4a4850e2a79
Lawrence Wang <setofvs@gmail.com>
2019-10-12 18:05:47 -0400

    Calling and Icon updates

--- components/Map.js
+++ components/Map.js
@@ -1,8 +1,17 @@
 import React from "react";
 import MapView, { Marker, Callout } from "react-native-maps";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Platform, Linking, Image } from "react-native";
 import { MaterialIcons } from "@expo/vector-icons";
 
+dialCall = number => {
+  let phoneNumber = "";
+  if (Platform.OS === "android") {
+    phoneNumber = `tel:${number}`;
+  } else {
+    phoneNumber = `telprompt:${number}`;
+  }
+  Linking.openURL(phoneNumber);
+};
 const Map = props => {
   return (
     <MapView
@@ -13,21 +22,26 @@ const Map = props => {
       style={{ flex: 1 }}
       region={props.region}
     >
-      <Marker coordinate={props.region} image={require("../assets/weOut.png")}>
+      <Marker
+        coordinate={props.region}
+        image={require("../assets/addEventIcon128.png")}
+        onPress={() => console.log("ADD AN EVENT")}
+      >
         {props.selected.description ? (
-          <Callout>
+          <Callout
+            style={styles.callout}
+            onPress={() => dialCall(props.details.formatted_phone_number)}
+          >
             <View style={styles.callout}>
               <Text style={{ padding: 2 }}>
-                <Text style={{ padding: 2 }}>
-                  {props.details.name}
-                  {"\n"}
-                  {props.details.formatted_address}
-                  {"\n"}
-                  {props.details.formatted_phone_number}
-                  {"\nOpening Hours:"}
-                  {"\n"}
-                  {props.details.opening_hours.weekday_text.join("\n")}
-                </Text>
+                {props.details.name}
+                {"\n"}
+                {props.details.formatted_address}
+                {"\n"}
+                {props.details.formatted_phone_number}
+                {"\nOpening Hours:"}
+                {"\n"}
+                {props.details.opening_hours.weekday_text.join("\n")}
               </Text>
               <MaterialIcons
                 name="add-circle"
